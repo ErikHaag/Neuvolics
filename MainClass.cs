d1e20793791a680b446d626518289ea81b8ecc28
green <green@COMPITER>
2026-01-25 22:54:48 -0600

    Add project files.

---
+++ MainClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Quintessential;
+
+namespace Neuvolics
+{
+    public class MainClass : QuintessentialMod
+    {
+        const string LogPrefix = "Neuvolics: ";
+
+
+        public override void Load()
+        {
+            Console.WriteLine(LogPrefix + "Loaded!");
+        }
+
+        public override void LoadPuzzleContent()
+        {
+            // second, import your assets, glyphs, transmutations, hooks, and detours
+        }
+
+        public override void PostLoad()
+        {
+            // third, follow through with various hand-offs
+        }
+
+        public override void Unload()
+        {
+            // lastly, remove all the hooks you made in LoadPuzzleContent
+        }
+    }
+}
